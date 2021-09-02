474f72dec9e60e9871f0478372a7940bff715d4f
belmirofss <belmirofss@gmail.com>
2021-09-02 18:32:02 -0300

    refactoring colors

--- src/constants/Colors.constants.tsx
+++
@@ -1,6 +0,0 @@
-export class Colors {
-
-    static readonly PRIMARY = '#ff914d';
-    static readonly SECONDARY = '#ffde59';
-    static readonly TERTIARY = '#ff1616';
-}
---
+++ src/theme/Colors.constant.tsx
@@ -0,0 +1,5 @@
+export const COLORS = {
+    PRIMARY: '#ff914d',
+    SECONDARY: '#ffde59',
+    TERTIARY: '#ff1616'
+}
