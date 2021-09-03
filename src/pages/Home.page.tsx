146f49ce9c425ff592bbe361e8858985ea4bc345
belmirofss <belmirofss@gmail.com>
2021-09-02 21:55:39 -0300

    added homepage message

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import AutoHeightImage from 'react-native-auto-height-image';
 import ButtonComponent from '../components/Button.component';
 import LOGO from '../images/logo.png';
@@ -8,11 +8,15 @@ export default function HomePage() {
 
   return (
     <View style={styles.container}>
-      <View>
+      <View style={styles.wrapperUpperContent}>
         <AutoHeightImage
           source={LOGO}
           width={264} 
         />
+
+        <Text style={styles.messageText}>
+          Play the classic snake game in a different way. Use your smartphone0 sensors to move the snake. Shake your smartphone!
+        </Text>
       </View>
 
       <View style={styles.wrapperButton}>
@@ -28,9 +32,16 @@ export default function HomePage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     flexDirection: 'column',
     justifyContent: 'space-between'
+  },
+  wrapperUpperContent: {
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  messageText: {
+    fontSize: 18,
+    textAlign: 'center'
   },
   wrapperButton: {
     width: '100%'
