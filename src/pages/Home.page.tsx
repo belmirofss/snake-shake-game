cd9790f52e114c13f660d0fb87cb7e5b33d67eaa
belmirofss <belmirofss@gmail.com>
2023-07-23 19:32:24 -0300

    dependencies

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,35 +1,31 @@
-import { useNavigation } from '@react-navigation/core';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import AutoHeightImage from 'react-native-auto-height-image';
-import ButtonComponent from '../components/Button.component';
-import TextMessageComponent from '../components/TextMessage.component';
-import LOGO from '../images/logo.png';
+import { useNavigation } from "@react-navigation/core";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import ButtonComponent from "../components/Button.component";
+import TextMessageComponent from "../components/TextMessage.component";
+import LOGO from "../images/logo.png";
 
 export default function HomePage() {
-
   const navigation = useNavigation();
 
-  const startGameclick = () => navigation.navigate('GamePage');
+  const startGameclick = () => navigation.navigate("GamePage");
 
   return (
     <View style={styles.container}>
       <View style={styles.wrapperUpperContent}>
-        <AutoHeightImage
+        {/* <AutoHeightImage
           source={LOGO}
           width={228} 
-        />
+        /> */}
 
         <TextMessageComponent>
-          Play the classic snake game in a different way. Use your smartphone sensors to move the snake. Shake your smartphone!
+          Play the classic snake game in a different way. Use your smartphone
+          sensors to move the snake. Shake your smartphone!
         </TextMessageComponent>
       </View>
 
       <View style={styles.wrapperButton}>
-        <ButtonComponent
-          text="START"
-          onPress={startGameclick} 
-        />
+        <ButtonComponent text="START" onPress={startGameclick} />
       </View>
     </View>
   );
@@ -38,14 +34,14 @@ export default function HomePage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'space-between'
+    flexDirection: "column",
+    justifyContent: "space-between",
   },
   wrapperUpperContent: {
-    flexDirection: 'column',
-    alignItems: 'center'
+    flexDirection: "column",
+    alignItems: "center",
   },
   wrapperButton: {
-    width: '100%'
-  }
+    width: "100%",
+  },
 });
